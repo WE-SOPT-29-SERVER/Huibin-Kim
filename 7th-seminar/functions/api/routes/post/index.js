a067db16ebd80179d85bd01e123a72934c8dc928
kmebin <blowhuiee@gmail.com>
2021-12-24 17:07:50 +0900

    [add] level1: 유저 인증 middleware 추가

--- 7th-seminar/functions/api/routes/post/index.js
+++ 7th-seminar/functions/api/routes/post/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
 router.get('/list', require('./postListGET'));
-router.get('/:postId', require('./postGET'));
+// 유저 인증이 필요한 요청 사이에 checkUser 미들웨어를 넣어준다
+router.get('/:postId', checkUser, require('./postGET'));
 router.put('/:postId', require('./postPUT'));
 router.delete('/:postId', require('./postDELETE'));
 router.post('/', require('./postPOST'));
